7822675fe42c892ca9b6a0eec3dc1452d225d53e
Tom Hickey <thickey@thickey.com>
2015-05-18 14:23:07 +0100

    Controller to handle image lookup and 404 errors

--- htdocs/controller.php
+++ htdocs/controller.php
@@ -1,50 +1,31 @@
 <?php
 
-//phpinfo(35);
-
 if(! isset($GLOBALS['site_name']))
 {
 	require($_SERVER["FILE_ROOT"].'classes/class.init_site.php');
 	$site = new site();
 }
 
+	require($GLOBALS['class_path'].'class.search_image.php');
+	require($GLOBALS['class_path'].'class.resize_image.php');
+
 /*
 	ok so this is where we end up if we get a 404 error.
 	so we need to take a look at the error url and see if
 	we can match that up to a file on our database
-
-	This is very much a proof of concept, you could just
-	search the filesystem for the image but I am assuming
-	that you are not in fact just throwing a load of images
-	into a single directory and there is some further organisation
-	in place for the images.  If not things will get confusing quickly!
 */
 
-//butcher up the URL to see if it's one of our images or not.
-$requested_image = explode('/',$_SERVER["REDIRECT_URL"]);
+	//search for the image first
+	$source_image = new search_image($_SERVER["REDIRECT_URL"]);
 
-//so we currently don't care about the much of the url at this point
-//we just want the end url part.
-$temp_array = array_reverse($requested_image);
-$requst_filename = array_shift($temp_array);
-
-//great we have our filename, now to search the database
-$sql = "select
-			source_images.source_filename
-		from
-			source_images
-		where
-			source_images.source_filename = :requst_filename
-		limit 1";
-
-$sql = $GLOBALS['database']->prepare($sql);
-$sql->bindValue(':requst_filename', $requst_filename, PDO::PARAM_STR);
-$sql->execute();
-
-$image_data = $sql->fetch(PDO::FETCH_ASSOC);
-
-print_r($image_data);
-
-//phpinfo(35);
-
+	if ($source_image->image_found != 1)
+	{
+		//we throw a normal 404 error as we can't find the image.
+		header("HTTP/1.0 404 Not Found");
+	}
+	else
+	{
+		//ok so we have an image we need to resize it first
+		$new_image = new resize_image($source_image);
+	}
 ?>
