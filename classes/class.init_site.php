d4d16b57b5fe9a80edc5b7d747e6ec83e2b84654
Tom Hickey <thickey@thickey.com>
2015-05-18 14:21:46 +0100

    Added / changed some config variables

--- classes/class.init_site.php
+++ classes/class.init_site.php
@@ -8,8 +8,17 @@ class site
 		$GLOBALS['site_name']			= "PHP Smart Image";
 		$GLOBALS['root_path']			= $_SERVER["FILE_ROOT"];
 		$GLOBALS['class_path']			= $_SERVER["FILE_ROOT"].'classes/';
-		$GLOBALS['soruce_image_path']	= $_SERVER["FILE_ROOT"].'image_source/';
+		$GLOBALS['soruce_image_path']	= $_SERVER["FILE_ROOT"].'source-images/';
 		$GLOBALS['dest_image_path']		= $_SERVER["FILE_ROOT"].'htdocs/images/';
+
+		//Image variabled
+		$GLOBALS['image_max_size']		= 2000;
+		$GLOBALS['image_highdpi_size']	= 2; //the multiplier for the retina image
+
+		$GLOBALS['image_compression']	= 60; //you may want to finesse this setting to suit
+		$GLOBALS['highdpi_compression']	= 30; //same as above, but you should be able to get away with more compression
+		$GLOBALS['highdpi_addition'] 	= '@2x'; //what to add to the filename at the end to signify a highdpi image
+
 
 		//Database config for me is set via enviroment variables
 		$GLOBALS['db_conf']['host']					= $_SERVER["DBHOST"];
@@ -22,5 +31,5 @@ class site
 		$database = new site_database();
 	}
 }
-	
+
 ?>
